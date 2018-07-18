675af47dc3c57f6fd8eea3387da346fcf38cd742
Hiroki Kato <hkato@sas.upenn.edu>
2018-07-18 17:00:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,16 @@
 class Dog
 
   @@all = Array.new 
+  attr_accessor :name
   
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
+  end
 
 end
 
