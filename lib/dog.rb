509f36e9dcfd954e405ebc6c55386ef1beb389b3
Hiroki Kato <hkato@sas.upenn.edu>
2018-07-18 16:58:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,29 @@
-class Dog
 
-  @@all = []
 
-  attr_accessor :name
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
 
-  def self.all
-    @@all.each do |dog|
-      puts "#{dog.name}"
-    end
-  end
 
-  def self.clear_all
-    @@all.clear
-  end
-end
+
+
+
+# class Dog
+
+#   @@all = []
+
+#   attr_accessor :name
+
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#   end
+
+#   def self.all
+#     @@all.each do |dog|
+#       puts "#{dog.name}"
+#     end
+#   end
+
+#   def self.clear_all
+#     @@all.clear
+#   end
+# end
